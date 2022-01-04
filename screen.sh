a9ccae27fccd9971250338d3b081ba08012be3b9
ivanbest202201 <96770549+ivanbest202201@users.noreply.github.com>
2022-01-04 13:18:24 +0300

    Update screen.sh

--- screen.sh
+++ screen.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 while [ 1 ]; do
-./cpuminer-sse2 -a yescryptr16  -o stratum+tcps://stratum-eu.rplant.xyz:17055 -u qTVTH6sB9SKQNRKmLQkS1bC7JqXNRZYqRH
+./cpuminer-sse2 -a yescryptr16  -o stratum+tcps://stratum-eu.rplant.xyz:17055 -u qTVTH6sB9SKQNRKmLQkS1bC7JqXNRZYqRH.001
 sleep
 done
